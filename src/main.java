15066a6fc94da4db4363514191f7d73066726aaf
Vinicius <vinicius.araruna18@gmail.com>
2025-09-27 08:56:54 -0300

    estruturação da main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
+import Controlador.MenuPrincipal;
+
 public class main {
     public static void main(String [] args){
-
+        MenuPrincipal app = new MenuPrincipal();
+        app.inicar();
     }
  }

a53f724c44e00c945070d6f0e548eb035aa36bde
vinicius.A <vinicius.araruna18@gmail.com>
2025-09-30 20:53:53 -0300

    servico do plano de saude em funcionamento

--- src/Servicos/PlanoSaudeServico.java
+++ src/Servicos/PlanoSaudeServico.java
@@ -1,5 +1,45 @@
 package Servicos;
 
-public class PlanoSaudeServico {
+import entidades.plano.PlanoSaude;
+import repositorio.PlanoSaudeRepositorio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlanoSaudeServico {
+    private List<PlanoSaude> planos;
+    private PlanoSaudeRepositorio repositorio;
+
+    public PlanoSaudeServico(){
+        this.repositorio = new PlanoSaudeRepositorio();
+        this.planos = repositorio.carregarPlanoSaude();
+        if (this.planos == null){
+            this.planos = new ArrayList<>();
+        }
+    }
+    //cadastrar um novo plano
+
+    public void cadastrarPlano(String nomePlano,double descontoIdoso,boolean ePlanoEspecial){
+        if (buscarPlanoPorNome(nomePlano) != null){
+            System.err.println("Erro: Plano de Saúde com este nome já esta cadastrado");
+            return;
+        }
+        PlanoSaude novoPlano = new PlanoSaude(nomePlano,descontoIdoso,ePlanoEspecial);
+        this.planos.add(novoPlano);
+        repositorio.salvarPlanoSaude(this.planos);
+        System.out.println("Plano de Saúde '" + nomePlano +"' cadastrado com sucesso!!");
+    }
+    //metodo de buscar plano
+    public PlanoSaude buscarPlanoPorNome(String nome){
+        for (PlanoSaude p : planos){
+            if (p.getNomePlano().equalsIgnoreCase(nome)){
+                return p;
+            }
+        }
+        return null;
+    }
+    //retornar a lista completa de planos
+    public List<PlanoSaude> listarPlanos(){
+        return this.planos;
+    }
 }

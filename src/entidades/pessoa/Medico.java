6b32784f60e7b90240724be85abad551506a4997
vinicius.A <vinicius.araruna18@gmail.com>
2025-10-03 02:02:02 -0300

    remocao do set custo consulta,especialidade,crm ja que eles nao devem ser alterados

--- src/entidades/pessoa/Medico.java
+++ src/entidades/pessoa/Medico.java
@@ -23,27 +23,15 @@ public class Medico extends Pessoa {
     public String getCRM(){
         return CRM;
     }
-    public void setCRM(String CRM){
-        this.CRM = CRM;
-    }
     public String getEspecialidade(){
         return especialidade;
     }
-    public void setEspecialidade(String especialidade){
-        this.especialidade = especialidade;
-    }
-
     public double getCustoConsulta() {
         return custoConsulta;
     }
-    public void setCustoConsulta(double custoConsulta){
-        this.custoConsulta = custoConsulta;
-    }
-
     public List<Consulta> getAgenda() {
         return agenda;
     }
-
     public void adicionarConsulta(Consulta consulta){
         this.agenda.add(consulta);
     }
